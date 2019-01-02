0648aa9b76586d533547b95587d09f8c3052a3df
Mark O'Connor <lemoco@hotmail.ca>
2019-01-02 13:58:40 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcome: 'Welcome',
+        currentDate: new Date().getFullYear()
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+        currentDate: new Date().getFullYear()
+    })
+});
+
+
+
+app.get('/bad', (req, res) => {
+    res.send({
+        'errorMessage': 'Too much'
+    })
+});
+
+app.listen(3000);
